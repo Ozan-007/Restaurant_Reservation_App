34d312cf040304a248ca421c002dec299c1ae9dd
Ozan <ozan28061@gmail.com>
2022-01-13 04:24:46 +0100

    Overriding usercreation form as SignUpForm

--- reservation/views.py
+++ reservation/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.template import context
-from django.contrib.auth.forms import UserCreationForm 
+from .forms import SignUpForm
 # Create your views here.
 def index(request):
     context={}
     return render(request,"index.html",context)
 
 def signup(request):
-    context ={'form': UserCreationForm()}
+    context ={'form': SignUpForm()}
     return render(request,"signup.html",context)
