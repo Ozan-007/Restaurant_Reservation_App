553d1ea2d08f320c24df845b3b0244b906116728
Ozan <ozan28061@gmail.com>
2022-01-13 03:06:40 +0100

    Table model created

--- reservation/models.py
+++ reservation/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+PARTY_SIZES = [
+    (2, 1),
+    (3,4),
+    (5, 6),
+]
+
+
+class Table(models.Model):
+    party_size = models.IntegerField(choices=PARTY_SIZES)
+    available_date = models.DateField()
+    available_time = models.TimeField()
+
+    def __str__(self):
+        return f"Table Number {self.id}"
+    
