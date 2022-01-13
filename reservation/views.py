4f7657d2d2fefe3d051de3a8ac86a36bf6116d07
Ozan <ozan28061@gmail.com>
2022-01-13 04:03:30 +0100

    signup.html and signup view created

--- reservation/views.py
+++ reservation/views.py
@@ -3,5 +3,9 @@ from django.template import context
 from django.contrib.auth.forms import UserCreationForm 
 # Create your views here.
 def index(request):
-    context ={'form': UserCreationForm()}
+    context={}
     return render(request,"index.html",context)
+
+def signup(request):
+    context ={'form': UserCreationForm()}
+    return render(request,"signup.html",context)
