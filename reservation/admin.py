5aaa9c0b57b5aa36492e7c2ecab936063b87796f
Ozan <ozan28061@gmail.com>
2022-01-13 03:11:08 +0100

    Party sizes updated and model adde to admin.py

--- reservation/admin.py
+++ reservation/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from reservation.models import Table
+
 # Register your models here.
+admin.site.register(Table)
--- reservation/models.py
+++ reservation/models.py
@@ -3,9 +3,11 @@ from django.db import models
 # Create your models here.
 
 PARTY_SIZES = [
-    (2, 1),
-    (3,4),
-    (5, 6),
+    (1,"1"),
+    (2, "2"),
+    (4, "3"),
+    (6, "4"),
+    (8 , "8")
 ]
 
 
