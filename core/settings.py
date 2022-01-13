c4bb23d7b16b44dd7ab5e63c9e5e6666dce47952
Ozan <ozan28061@gmail.com>
2022-01-13 05:11:36 +0100

    Sign Up page's structure has finished

--- core/settings.py
+++ core/settings.py
@@ -40,8 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     #my-apps
     'reservation',
+    'crispy_forms',
 
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- reservation/views.py
+++ reservation/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.template import context
 from .forms import SignUpForm
+from django.contrib import messages
 # Create your views here.
 def index(request):
     context={}
     return render(request,"index.html",context)
 
 def signup(request):
-    context ={'form': SignUpForm()}
+    register_form = SignUpForm()
+    if request.method == "POST":
+        register_form = SignUpForm(request.POST)
+    if register_form.is_valid():
+        register_form.save()
+        username = register_form.cleaned_data.get('username')
+        messages.success(request, f'Account created for {username}.')
+        return redirect('index')
+    context ={'signupform': SignUpForm()}
     return render(request,"signup.html",context)
