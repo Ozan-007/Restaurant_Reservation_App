6c5d13bbdfc81246b7b0f76dc6b29e1f23d53a89
Ozan <ozan28061@gmail.com>
2022-01-18 20:29:45 +0100

    reservation form created , reservate view created

--- reservation/forms.py
+++ reservation/forms.py
@@ -1,7 +1,17 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from .models import Table
+#Forms
+
 class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username','email', 'password1','password2']
+
+
+
+class TableForm(forms.ModelForm):
+    class meta:
+        model = Table
+        fields = "__all__"
--- reservation/views.py
+++ reservation/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render, redirect
 from django.template import context
-from .forms import SignUpForm
+from .forms import SignUpForm, TableForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 def home(request):
     context={}
     return render(request,"home.html",context)
+
+def reservate(request):
+    reservation_form =  TableForm()
+    if request.method == "POST":
+        reservation_form = TableForm(request.POST)
+        if reservation_form.is_valid():
+            reservation_form.save()
+            party_size = reservation_form.cleaned_data.get('party_size')
+            messages.success(request, f'Table for {party_size} people has been reserverd for you.')
+            return redirect('home')
+        else:
+           messages.error(request, reservation_form.errors)
+    context ={'reservation_form': TableForm()}
+    return render(request,"reservation.html",context)
+
+
 
 def signup(request):
     register_form = SignUpForm()
